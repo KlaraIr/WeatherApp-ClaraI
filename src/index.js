054fec0e8bc885aff6b9e84eeb0f1d3d7c452aa8
KlaraIr <ciribarne@educ.ar>
2025-01-15 12:25:07 +0100

    Added the search functionality with Api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function refreshWeather(response) {}
+
+function searchCity(city) {
+  let apiKey = "e5bba9f22a0387e15ceoed7411363t90";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
